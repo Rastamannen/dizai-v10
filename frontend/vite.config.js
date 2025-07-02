ea359931289bf67bd601fc830f2e7902baac3c69
Rastamannen <johan@laurelli.com>
2025-07-02 16:46:20 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API-förfrågningar till backend under utveckling
       '/api': 'http://localhost:3001',
       '/tts': 'http://localhost:3001',
       '/exercises': 'http://localhost:3001'
     }
   },
-  // Bygg ut till "dist" som standard (Render gillar det)
   build: {
     outDir: 'dist'
   }
