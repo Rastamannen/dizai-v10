823eabdc35ad4ae4183e3f55b6794b08eed13cbb
Rastamannen <johan@laurelli.com>
2025-07-04 00:19:35 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require('vite');
 const react = require('@vitejs/plugin-react');
+const path = require('path');
 
 module.exports = defineConfig({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
